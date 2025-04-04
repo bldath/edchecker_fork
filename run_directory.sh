f336c5959fe437019460d5374e2574728c48a8da
grahnen <samuel.grahn@it.uu.se>
2025-04-04 14:02:20 +0200

    Fix benchmark code

--- run_directory.sh
+++ run_directory.sh
@@ -1,6 +1,15 @@
-#!/usr/bin/env sh
+#!/bin/sh -x
 
-for i in ../nidhugg/benchmarks/event-driven/**/*.trace; do
-    echo $(dirname $i)
-    #cargo run $i
+
+heuristics=(no simple full)
+adt=(multiset queue stack)
+
+
+for h in "${heuristics[@]}"; do
+    for adt in "${adt[@]}"; do
+        #parallel "echo {}" ::: /home/grahnen/build/nidhugg/EDC-traces/*/*/*.trace
+        parallel --timeout 120 --bar "./target/release/z3checker ${adt} ${h} {} > {.}_${adt}_${h}.out" ::: /home/grahnen/nidhugg/EDC-traces/*/*/*.trace
+        
+
+    done
 done
