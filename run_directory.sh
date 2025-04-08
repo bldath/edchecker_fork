098d6cb5d302a58cdf4d729b6fdf008e7ee69ee0
grahnen <samuel.grahn@it.uu.se>
2025-04-08 11:57:05 +0200

    Fix run_directory

--- run_directory.sh
+++ run_directory.sh
@@ -7,12 +7,12 @@ adt=(multiset queue stack)
 for h in "${heuristics[@]}"; do
     for adt in "${adt[@]}"; do
         #parallel --bar "echo {.}" ::: /home/grahnen/nidhugg/EDC-traces/*/*/*.trace
-        parallel --rpl '{expt} s:(\.?/[-\w]+)*/([-\w]+/[-\w]+)\.\w+:\2:;' \
+        parallel --rpl '{expt} s:(\.*/[-_\w]+)*/([-_\w]+/[-_\w]+)\.trace:\2:;' \
                  --timeout 60 --bar \
                  "dirname {expt} | xargs -I% mkdir -p $target/%; ./target/release/z3checker ${adt} ${h} {} > $target/{expt}_z3_${adt}_${h}.out" \
                  ::: EDC-traces/*/*.trace
 
-        parallel --rpl '{expr} s:(\.?/[-\w]+)*/([-\w]+/[-\w]+)\.\w+:\2:;' \
+        parallel --rpl '{expt} s:(\.*/[-_\w]+)*/([-_\w]+/[-_\w]+)\.trace:\2:;' \
                  --timeout 60 --bar \
                  "dirname {expt} | xargs -I% mkdir -p $target/%; ./target/release/edchecker ${adt} ${h} {} > $target/{expt}_graph_${adt}_${h}.out" \
                  ::: EDC-traces/*/*.trace
