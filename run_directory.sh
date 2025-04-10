6783cb365967c9cf3f0f2feb90963d0f205417de
Samuel Grahn <samuel@grahnen.se>
2025-04-10 23:55:58 +0200

    Run JSON traces instead of custom format

--- run_directory.sh
+++ run_directory.sh
@@ -16,14 +16,14 @@ echo "Running benchmarks with timeout $timeout s"
 for h in "${heuristics[@]}"; do
     for adt in "${adt[@]}"; do
         #parallel --bar "echo {.}" ::: /home/grahnen/nidhugg/EDC-traces/*/*/*.trace
-        parallel --rpl '{expt} s:(\.*/*[-_\w]+)*/([-_\w]+/[-_\w]+)\.trace:\2:;' \
+        parallel --rpl '{expt} s:(\.*/*[-_\w]+)*/([-_\w]+/[-_\w]+)\.\w+:\2:;' \
                  --timeout $timeout --bar \
                  "dirname {expt} | xargs -I% mkdir -p $output/%; ./target/release/z3checker ${adt} ${h} {} > $output/{expt}_z3_${adt}_${h}.out" \
-                 ::: $target/*/*.trace
+                 ::: $target/*/*.json
 
-        parallel --rpl '{expt} s:(\.*/*[-_\w]+)*/([-_\w]+/[-_\w]+)\.trace:\2:;' \
+        parallel --rpl '{expt} s:(\.*/*[-_\w]+)*/([-_\w]+/[-_\w]+)\.\w+:\2:;' \
                  --timeout $timeout --bar \
                  "dirname {expt} | xargs -I% mkdir -p $output/%; ./target/release/edchecker ${adt} ${h} {} > $output/{expt}_graph_${adt}_${h}.out" \
-                 ::: $target/*/*.trace
+                 ::: $target/*/*.json
     done
 done
